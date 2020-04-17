00545bcef27c877859ff26c38af4d95829f70a87
James Vaughan <jvaughan@staffmail.ed.ac.uk>
2020-04-17 02:34:03 +0100

    Used lauch.config from electron-forge tutorial

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { app, BrowserWindow } from 'electron';
+
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
@@ -7,17 +8,17 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 }
 
 const createWindow = () => {
+
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    height: 600,
-    width: 800,
+  const window = new BrowserWindow({
+    height: 600, width: 800
   });
 
   // and load the index.html of the app.
-  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
+  window.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  window.webContents.openDevTools({ mode: "detach" });
 };
 
 // This method will be called when Electron has finished
