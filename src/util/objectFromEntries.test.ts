c611766ccba678ce63642ca5d95c86e4f4eb63f7
James Vaughan <James Vaughan>
2020-04-24 12:20:25 +0100

    Fixed objectFromEntries test

--- src/util/objectFromEntries.test.ts
+++ src/util/objectFromEntries.test.ts
@@ -2,6 +2,9 @@
 import { objectFromEntries } from './objectFromEntries';
 
 it('should create new objects from entries list', () => {
-    const newObj = objectFromEntries([["hello", 1], ["world", "!!!"], ["child", {meh: 3}]]);
-    expect(newObj).toEqual({ hello: 1, world: "!!!", child: { meh: 3 } });
+  const entries: [string, any][] = 
+    [["hello", 1], ["world", "!!!"], ["child", { meh: 3 }]];
+
+  expect(objectFromEntries(entries))
+    .toEqual({ hello: 1, world: "!!!", child: { meh: 3 } });
 });
