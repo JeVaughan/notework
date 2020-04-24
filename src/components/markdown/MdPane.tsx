057bdc08137c3b92ac867f7a5a1933e99e022d45
James Vaughan <jvaughan@staffmail.ed.ac.uk>
2020-04-24 15:57:00 +0100

    WIP new markdown elements

---
+++ src/components/markdown/MdPane.tsx
@@ -0,0 +1,15 @@
+
+export type MdPaneProps = {
+  markdown: string,
+  writeMd: (newMd: string) => void,
+};
+
+/**
+ * Root element for a block of markdown
+ * 
+ * 
+ * @param param0 
+ */
+export function MdPane(props: MdPaneProps) {
+
+}
