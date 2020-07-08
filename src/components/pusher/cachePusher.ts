90822d2f832e8c04f39eff7c5cc10da46a020d8a
James Vaughan <James Vaughan>
2020-07-08 15:18:44 +0100

    cachePusher implementation

--- src/components/pusher/cachePusher.ts
+++ src/components/pusher/cachePusher.ts
@@ -1,9 +1,31 @@
-import { PusherM } from "./Push";
 
-export function cachePusher<K, V>(
-  basePusher: PusherM<K, V>,
-  equivalence: (a: V, b: V) => boolean
+import { mapPromise } from "../../util/mapPromise";
+
+import { PusherM, Pushed, PPushed, pusherM } from "./Push";
+
+export function cachePusher<K, V, v extends V = V>(
+  basePusher: PusherM<K, v>,
+  merge: (src: v, ext: V) => v,
+  latestValue: v
 ): PusherM<K, V> {
 
-  return basePusher; // TODO
+  const handleResult = mapPromise(
+    function(result: Pushed<K, v>): Pushed<K, v> {
+      if (result.newValue) 
+        latestValue = result.newValue;
+
+      return result;
+    }
+  );
+
+  function pusherFn(baseVersion: K, update?: V): PPushed<K, V> {
+    return handleResult(
+      basePusher.push(
+        baseVersion, 
+        merge(latestValue, update)
+      )
+    );
+  }
+
+  return pusherM(pusherFn, () => basePusher.close());
 }
