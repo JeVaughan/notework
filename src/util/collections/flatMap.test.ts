292ee1581958cb9efae67d7a7098528be98ad8b6
James Vaughan <jvaughan@staffmail.ed.ac.uk>
2020-07-26 01:37:08 +0100

    Fixed broken test for flatMap

--- src/util/collections/flatMap.test.ts
+++ src/util/collections/flatMap.test.ts
@@ -2,11 +2,11 @@ import { identity } from "../identity"
 import { flatMap } from "./flatMap"
 import { Range } from "immutable";
 
-it('should', () => {
+it('should handle an empty list', () => {
   expect(flatMap([], identity)).toEqual([]);
 });
 
-it('should', () => {
-  expect(flatMap(Range(1, 4), i => Range(1, i)))
+it('should always return a flattened list', () => {
+  expect(flatMap(Range(1, 6), i => Range(1, i)))
     .toEqual([1, 1, 2, 1, 2, 3, 1, 2, 3, 4]);
 })
